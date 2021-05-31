d18ee62915edd533186960ceacd3d8b5f1b6729e
Josiah-090602 <viernesjosiah6@gmail.com>
2021-06-01 00:36:18 +0800

    init

---
+++ src/main/AWTAccumulator.java
@@ -0,0 +1,14 @@
+package main;
+
+public class AWTAccumulator {
+
+	public AWTAccumulator() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
